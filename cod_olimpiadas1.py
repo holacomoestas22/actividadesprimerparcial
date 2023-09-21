ea8dacf7ef19166ce03f93f4bc0ee5bae81d929c
holacomoestas22 <145036535+holacomoestas22@users.noreply.github.com>
2023-09-21 09:22:29 -0600

    cod_olimpiadas1.py ejercicio 1

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,16 @@
+def multi():
+multi= a*b
+x= a*b
+return = x
+
+def division():
+division= a/b
+x= a/b
+return = x
+print ("dame tu primer numero ")
+a= int(primer valor)
+print ("dame tu segundo numero ")
+b= int (segundo valor)
+
+print ("tu multi es", a*b)
+print ("tu dicision es", a/b)
